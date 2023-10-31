9a9f0b5e7bba6e13be8786a1a694b814f6be725b
Souleymane Sall <papa.sall243@gmail.com>
2023-10-31 11:38:34 +0100

    Comment the TokenObtainSerializer to fix issues. Real fixe later

--- digikazAPI/digikazAPI/settings.py
+++ digikazAPI/digikazAPI/settings.py
@@ -154,7 +154,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=60),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
-    "TOKEN_OBTAIN_SERIALIZER": "digikazAPI.properties.token.MyTokenObtainPairSerializer",
+    # "TOKEN_OBTAIN_SERIALIZER": "digikazAPI.properties.token.MyTokenObtainPairSerializer",
 }
 
 
